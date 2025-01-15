d488ae9206229518c52eb3b3117e9c155fa53977
daromi94 <davromero07@gmail.com>
2025-01-14 20:35:55 -0500

    feat: :sparkles: add episodes

--- src/core/shows/src/main/kotlin/com/daromi/playbook/core/shows/domain/Show.kt
+++ src/core/shows/src/main/kotlin/com/daromi/playbook/core/shows/domain/Show.kt
@@ -1,5 +1,6 @@
 package com.daromi.playbook.core.shows.domain
 
+import java.time.LocalDate
 import java.util.SortedMap
 
 class Show private constructor(
@@ -21,8 +22,15 @@ class Show private constructor(
     }
 }
 
-class Season private constructor(
+private class Season private constructor(
     private val number: SeasonNumber,
+    private val episodes: SortedMap<EpisodeNumber, Episode> = sortedMapOf(),
+)
+
+private data class Episode(
+    val number: EpisodeNumber,
+    val name: EpisodeName,
+    val releaseDate: EpisodeReleaseDate,
 )
 
 @JvmInline
@@ -68,3 +76,38 @@ private value class SeasonNumber(
 
     override fun compareTo(other: SeasonNumber): Int = this.value - other.value
 }
+
+@JvmInline
+private value class EpisodeNumber(
+    val value: Int,
+) : Comparable<EpisodeNumber> {
+    companion object {
+        @JvmStatic
+        fun from(value: Int): EpisodeNumber? {
+            if (value <= 0) return null
+
+            return EpisodeNumber(value)
+        }
+    }
+
+    override fun compareTo(other: EpisodeNumber): Int = this.value - other.value
+}
+
+@JvmInline
+private value class EpisodeName(
+    val value: String,
+) {
+    companion object {
+        @JvmStatic
+        fun from(value: String): EpisodeName? {
+            if (value.isBlank()) return null
+
+            return EpisodeName(value)
+        }
+    }
+}
+
+@JvmInline
+private value class EpisodeReleaseDate(
+    val value: LocalDate,
+)
